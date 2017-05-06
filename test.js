afe7aa698fcabedca61f278b6f1c09e7f87c9235
spences10 <spences10apps@gmail.com>
2017-05-06 07:03:49 +0100

    tidy unused files 🗑

--- test.js
+++
@@ -1,7 +0,0 @@
-var Spark = require('sparkai');
-
-var bot = new Spark.MarkovChain({
-  sentences: 1 // number of sentences (default = 3)
-})
-bot.train(['some data', 'more data']); // can be an array, or a single string
-console.log(bot.generate()); // generates text
