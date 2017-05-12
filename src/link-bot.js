5c60755522ec3b21931f837d057205071e17455f
spences10 <spences10apps@gmail.com>
2017-05-12 09:43:09 +0100

    No semi 🍆

--- src/link-bot.js
+++ src/link-bot.js
@@ -2,15 +2,15 @@ const Twit = require('twit')
 const config = require('./config')
 const Tabletop = require('tabletop')
 
-const bot = new Twit(config);
+const bot = new Twit(config)
 
-const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1842GC9JS9qDWHc-9leZoEn9Q_-jcPUcuDvIqd_MMPZQ/pubhtml';
+const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1842GC9JS9qDWHc-9leZoEn9Q_-jcPUcuDvIqd_MMPZQ/pubhtml'
 
 Tabletop.init({
   key: spreadsheetUrl,
   callback(data, tabletop) {
     data.forEach(d => {
-      const status = `${d.links} a link from a Google spreadsheet`;
+      const status = `${d.links} a link from a Google spreadsheet`
       console.log(status)
       bot.post('statuses/update', {
         status
