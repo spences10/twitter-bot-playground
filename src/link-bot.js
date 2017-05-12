2e145ebc373338ebfc17b5b0052de7afeac0f245
spences10 <spences10apps@gmail.com>
2017-05-12 09:40:43 +0100

    🏹 functions

--- src/link-bot.js
+++ src/link-bot.js
@@ -1,20 +1,20 @@
-var Twit = require('twit')
-var config = require('./config')
-var Tabletop = require('tabletop')
+const Twit = require('twit')
+const config = require('./config')
+const Tabletop = require('tabletop')
 
-var bot = new Twit(config)
+const bot = new Twit(config);
 
-var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1842GC9JS9qDWHc-9leZoEn9Q_-jcPUcuDvIqd_MMPZQ/pubhtml'
+const spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1842GC9JS9qDWHc-9leZoEn9Q_-jcPUcuDvIqd_MMPZQ/pubhtml';
 
 Tabletop.init({
   key: spreadsheetUrl,
-  callback: function (data, tabletop) {
-    data.forEach(function (d) {
-      var status = d.links + ' a link from a Google spreadsheet'
+  callback(data, tabletop) {
+    data.forEach(d => {
+      const status = `${d.links} a link from a Google spreadsheet`;
       console.log(status)
       bot.post('statuses/update', {
-        status: status
-      }, function (err, response, data) {
+        status
+      }, (err, response, data) => {
         if (err) {
           console.log(err)
         } else {
