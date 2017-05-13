bf85daf38d1dd901db4859227601352c8fcfb9c1
spences10 <spences10apps@gmail.com>
2017-05-13 07:22:22 +0100

    intervals set ⌛

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,12 @@ const markov = require('./markov-bot')
 const link = require('./link-bot')
 
 picture()
-// setInterval(picture, 1000 * 60 * 60 * 24)
+setInterval(picture, 1000 * 60 * 60 * 24)
 
-// const markovInterval = Math.floor(Math.random() * 180) + 5
-// markov()
-// setInterval(markov, markovInterval)
+const markovInterval = Math.floor(Math.random() * 180) + 5
+markov()
+setInterval(markov, markovInterval)
 
 
-// link()
-// setInterval(link, 1000 * 60 * 60 * 24)
+link()
+setInterval(link, 1000 * 60 * 60 * 24)
